17a73f19bc25edd3edfd6567bd0c24fa03bcaad1
GMO <palacin.guillermo@gmail.com>
2018-12-25 18:25:41 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+/* Inicializar el juego */
+function iniciarJuego() {
+  /* TODO */
+}
+
+/* Convierte lo que hay en el mapa en un archivo de imagen */
+function mapaToImg(x, y) {
+  /* TODO */
+}
